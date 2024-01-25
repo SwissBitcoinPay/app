be12fed36107a2bdc3112d1bc42d71b235368884
Swiss Bitcoin Pay <info@swiss-bitcoin-pay.ch>
2024-01-26 00:00:12 +0100

    check if app is native too

--- src/components/Picker/components/PickerRoot.tsx
+++ src/components/Picker/components/PickerRoot.tsx
@@ -1,8 +1,8 @@
 import { Picker as RootPicker, PickerIOS } from "@react-native-picker/picker";
 import { platform } from "@config";
 
-const { isIos } = platform;
+const { isIos, isNative } = platform;
 
-export const PickerRoot = (isIos
+export const PickerRoot = (isIos && isNative
   ? PickerIOS
   : RootPicker) as unknown as typeof RootPicker;
