36b47fb190d0980a553dd4b052619365e742b77c
Swiss Bitcoin Pay <info@swiss-bitcoin-pay.ch>
2024-12-20 11:49:28 +0100

    Add fiat rate to printer tickets

--- src/utils/Printer/Printer.native.ts
+++ src/utils/Printer/Printer.native.ts
@@ -6,21 +6,20 @@ import {
   NyxFont
 } from "react-native-nyx-printer";
 
-export const printLabelValue = async (
-  label: string,
-  value: string,
-  customStyle = {}
-) => {
+export const printLabelValue = async (label: string, value: string) => {
   try {
-    const TOTAL_WIDTH = !customStyle ? 20 : 27;
+    const TOTAL_WIDTH = 27;
     const SPACE = " ";
+    const THOUSAND_SPACE = " ";
+
+    // eslint-disable-next-line no-irregular-whitespace
+    const thousandsSpaceNb = value.match(/ /g)?.length || 0;
 
     const text = `${label}${SPACE.repeat(
       TOTAL_WIDTH - label.length - value.length
-    )}${value}`;
+    )}${THOUSAND_SPACE.repeat(thousandsSpaceNb)}${value}`;
     await printText(text, {
-      font: NyxFont.monospace,
-      ...customStyle
+      font: NyxFont.monospace
     });
   } catch (e) {
     console.error("error", e);
