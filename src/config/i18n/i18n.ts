b426067c855d3ba8f229d77060cad2a6a3e38db9
Swiss Bitcoin Pay <info@swiss-bitcoin-pay.ch>
2024-12-28 22:49:47 +0100

    minor language detector update

--- src/config/i18n/i18n.ts
+++ src/config/i18n/i18n.ts
@@ -1,6 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
+import { LanguageDetector } from "./language-detector";
 import { i18nConfig } from "./i18n-config";
 
 void i18n.use(LanguageDetector).use(initReactI18next).init(i18nConfig);
---
+++ src/config/i18n/language-detector/language-detector.ts
@@ -0,0 +1,3 @@
+import LanguageDetector from "i18next-browser-languagedetector";
+
+export { LanguageDetector };
