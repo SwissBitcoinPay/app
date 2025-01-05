1e689ca440c278c2fccdc8ce001f8e20103b352a
Swiss Bitcoin Pay <info@swiss-bitcoin-pay.ch>
2025-01-05 11:26:37 +0100

    fallback

--- src/utils/getFormattedUnit.ts
+++ src/utils/getFormattedUnit.ts
@@ -50,9 +50,10 @@ export const getUnitPrefixAndSuffix = (unit: string) => {
   return { unitPrefix, unitSuffix };
 };
 
-export const decimalSeparator = Intl.NumberFormat(undefined)
-  .formatToParts(1.1)
-  .find((part) => part.type === "decimal").value;
+export const decimalSeparator =
+  Intl.NumberFormat(undefined)
+    ?.formatToParts(1.1)
+    ?.find((part) => part.type === "decimal")?.value || ",";
 
 // Used for keyboard input on web
 export const decimalSeparatorNameMapping = {
