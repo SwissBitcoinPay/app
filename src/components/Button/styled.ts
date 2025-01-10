fb3e38be4894cf0754fc2c8b4ad1bdcac5544b3f
Swiss Bitcoin Pay <info@swiss-bitcoin-pay.ch>
2025-01-10 17:16:30 +0100

    Fix open url crash

--- src/components/Button/styled.ts
+++ src/components/Button/styled.ts
@@ -82,6 +82,7 @@ export const Button = styled(Pressable)<{
 
 export const ButtonContent = styled(View)<{ size: Size }>`
   height: 100%;
+  width: 100%;
   box-sizing: border-box;
   padding: 0px ${({ size }) => getPadding(size)}px;
   display: flex;
