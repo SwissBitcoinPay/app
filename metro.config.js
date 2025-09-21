3170bffd3f6088b9eb14bbc74c311593d6839f4b
Swiss Bitcoin Pay <hello@swiss-bitcoin-pay.ch>
2025-09-21 09:56:05 +0200

    running on android dev

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,5 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-const {
-  withSentryConfig
-} = require("@sentry/react-native/metro");
-
 const defaultConfig = getDefaultConfig(__dirname);
 
 const { assetExts, sourceExts } = defaultConfig.resolver;
@@ -27,4 +23,4 @@ const config = {
   }
 };
 
-module.exports = withSentryConfig(mergeConfig(defaultConfig, config));
+module.exports = mergeConfig(defaultConfig, config);
