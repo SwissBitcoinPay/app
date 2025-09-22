114403acd9e5d71f342e61b108a87b0b0291d7a6
Swiss Bitcoin Pay <hello@swiss-bitcoin-pay.ch>
2025-09-22 12:33:12 +0200

    remove sentry wrapper

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,5 @@
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
-const { withSentryConfig } = require("@sentry/react-native/metro");
-
 const defaultConfig = getDefaultConfig(__dirname);
 
 const { assetExts, sourceExts } = defaultConfig.resolver;
@@ -25,4 +23,4 @@ const config = {
   }
 };
 
-module.exports = withSentryConfig(mergeConfig(defaultConfig, config));
+module.exports = mergeConfig(defaultConfig, config);
