4f4ff65222683f0bd408ab47e8793c1f414422db
Swiss Bitcoin Pay <hello@swiss-bitcoin-pay.ch>
2025-09-27 15:54:25 +0200

    Added hardware wallet links

--- src/components/ConnectWalletModal/components/Empty/Empty.tsx
+++ src/components/ConnectWalletModal/components/Empty/Empty.tsx
@@ -1,6 +1,6 @@
 import { useContext, useMemo } from "react";
 import { useTranslation } from "react-i18next";
-import { ComponentStack } from "@components";
+import { ComponentStack, Url } from "@components";
 import * as ConnectStyled from "../../styled";
 import * as S from "./styled";
 import {
@@ -10,6 +10,7 @@ import {
   SBPHardwareWalletContext
 } from "@config/SBPHardwareWallet";
 import { useIsScreenSizeMin } from "@hooks";
+import { hardwareNames } from "@utils";
 
 export const Empty = () => {
   const isLarge = useIsScreenSizeMin("large");
@@ -27,61 +28,83 @@ export const Empty = () => {
       <ConnectStyled.Title>{t("title")}</ConnectStyled.Title>
       <S.SelectHardwareContainer
         direction={isLarge ? "horizontal" : "vertical"}
+        gapSize={26}
       >
-        <S.HardwareContainer
-          disabled={!IS_LEDGER_SUPPORTED}
-          onPress={() => {
-            setHardwareType("ledger");
-          }}
-        >
-          <S.HardwareImage
-            source={require("@assets/images/ledger.png")}
-            style={{ aspectRatio: "3/1", transform: [{ scale: imageRatio }] }}
-          />
-          {!IS_LEDGER_SUPPORTED && (
-            <S.NotSupportedText>
-              {tRoot("common.notSupported")}
-            </S.NotSupportedText>
-          )}
-        </S.HardwareContainer>
-        <S.HardwareContainer
-          disabled={!IS_BITBOX_SUPPORTED}
-          onPress={() => {
-            setHardwareType("bitbox02");
-          }}
-        >
-          <S.HardwareImage
-            source={require("@assets/images/bitbox.png")}
-            style={{
-              aspectRatio: "337/83",
-              transform: [{ scale: imageRatio * 0.8 }]
+        <S.HardwareContainerWrapper>
+          <S.HardwareContainer
+            disabled={!IS_LEDGER_SUPPORTED}
+            onPress={() => {
+              setHardwareType("ledger");
             }}
+          >
+            <S.HardwareImage
+              source={require("@assets/images/ledger.png")}
+              style={{ aspectRatio: "3/1", transform: [{ scale: imageRatio }] }}
+            />
+            {!IS_LEDGER_SUPPORTED && (
+              <S.NotSupportedText>
+                {tRoot("common.notSupported")}
+              </S.NotSupportedText>
+            )}
+          </S.HardwareContainer>
+          <Url
+            as={S.BuyHardwareText}
+            title={`${t("buyHardware", { hardwareName: hardwareNames["ledger"] })} 🇫🇷`}
+            href="https://shop.ledger.com/?r=1fbe532b2688&tracker=sbp-signup"
           />
-          {!IS_BITBOX_SUPPORTED && (
-            <S.NotSupportedText>
-              {tRoot("common.notSupported")}
-            </S.NotSupportedText>
-          )}
-        </S.HardwareContainer>
-        <S.HardwareContainer
-          disabled={!IS_TREZOR_SUPPORTED}
-          onPress={() => {
-            setHardwareType("trezor");
-          }}
-        >
-          <S.HardwareImage
-            source={require("@assets/images/trezor.png")}
-            style={{
-              aspectRatio: "192/49",
-              transform: [{ scale: imageRatio * 0.82 }]
+        </S.HardwareContainerWrapper>
+        <S.HardwareContainerWrapper>
+          <S.HardwareContainer
+            disabled={!IS_BITBOX_SUPPORTED}
+            onPress={() => {
+              setHardwareType("bitbox02");
             }}
+          >
+            <S.HardwareImage
+              source={require("@assets/images/bitbox.png")}
+              style={{
+                aspectRatio: "337/83",
+                transform: [{ scale: imageRatio * 0.8 }]
+              }}
+            />
+            {!IS_BITBOX_SUPPORTED && (
+              <S.NotSupportedText>
+                {tRoot("common.notSupported")}
+              </S.NotSupportedText>
+            )}
+          </S.HardwareContainer>
+          <Url
+            as={S.BuyHardwareText}
+            title={`${t("buyHardware", { hardwareName: hardwareNames["bitbox02"] })} 🇨🇭`}
+            href="https://shop.bitbox.swiss/?ref=KerwHOKfOK"
           />
-          {!IS_TREZOR_SUPPORTED && (
-            <S.NotSupportedText>
-              {tRoot("common.notSupported")}
-            </S.NotSupportedText>
-          )}
-        </S.HardwareContainer>
+        </S.HardwareContainerWrapper>
+        <S.HardwareContainerWrapper>
+          <S.HardwareContainer
+            disabled={!IS_TREZOR_SUPPORTED}
+            onPress={() => {
+              setHardwareType("trezor");
+            }}
+          >
+            <S.HardwareImage
+              source={require("@assets/images/trezor.png")}
+              style={{
+                aspectRatio: "192/49",
+                transform: [{ scale: imageRatio * 0.82 }]
+              }}
+            />
+            {!IS_TREZOR_SUPPORTED && (
+              <S.NotSupportedText>
+                {tRoot("common.notSupported")}
+              </S.NotSupportedText>
+            )}
+          </S.HardwareContainer>
+          <Url
+            as={S.BuyHardwareText}
+            title={`${t("buyHardware", { hardwareName: hardwareNames["trezor"] })} 🇨🇿`}
+            href="https://affil.trezor.io/aff_c?offer_id=235&aff_id=36313&source=sbp-signup"
+          />
+        </S.HardwareContainerWrapper>
       </S.SelectHardwareContainer>
     </ComponentStack>
   );
